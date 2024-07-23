cbff265ecb92ef8740b23f50562ede64e092b681
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-23 23:56:11 +0530

    useEffect and Controller

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+const LoadingSpinner = () => {
+    return (
+        <div className="loading-spinner my-3">
+            <div className="spinner-border text-primary fw-bold" role="status" style={{ width: '5rem', height: '5rem' }}>
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    );
+};
+export default LoadingSpinner;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,25 +1,37 @@
-import { useContext,useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
+import LoadingSpinner from "./LoadingSpinner";
 import WelcomeMessage from "./WelcomeMessage";
 import Post from "./Post";
 const PostList = () => {
     const { postList, addInitialPosts } = useContext(PostListData);
-    const [dataFetched, setDataFetched] = useState(false);
-    if (!dataFetched) {
-        fetch('https://dummyjson.com/posts')
+    const [fetching,setFetching]= useState(false);
+    useEffect(() =>{
+        setFetching(true);
+        const controller = new AbortController();
+        const signal = controller.signal;
+        fetch('https://dummyjson.com/posts', {signal})
             .then(res => res.json())
-            .then((data) => addInitialPosts(data.posts));
-        setDataFetched(true);
-    }
+            .then((data) =>{
+                addInitialPosts(data.posts);
+                setFetching(false);
+            });
+
+            return () => {
+                console.log('cleanup');
+                controller.abort();
+            }
+        }, []);
     return (
         <>
             <h1 className="text-center text-3xl text-rose-200 my-2">ALL POSTS</h1>
-            {postList.length === 0 && <WelcomeMessage />}
-            <div className="flex flex-row flex-wrap-reverse">
+            {fetching && <LoadingSpinner />}
+            {!fetching && postList.length === 0 && <WelcomeMessage />}
+            {!fetching && <div className="flex flex-row flex-wrap-reverse justify-center ">
                 {postList.map((post) => {
                     return <Post key={post.id} post={post} />;
                 })}
-            </div>
+            </div>}
         </>
     )
 };
