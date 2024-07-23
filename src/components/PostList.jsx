b7edb36def74c48b0111fcdc69b3e2f211d7ecca
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-23 22:57:46 +0530

    using useState to fetch data directly

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,18 +1,20 @@
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 import Post from "./Post";
 const PostList = () => {
     const { postList, addInitialPosts } = useContext(PostListData);
-    const handleGetPostsClick = () => {
+    const [dataFetched, setDataFetched] = useState(false);
+    if (!dataFetched) {
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then((data) => addInitialPosts(data.posts));
-    };
+        setDataFetched(true);
+    }
     return (
         <>
             <h1 className="text-center text-3xl text-rose-200 my-2">ALL POSTS</h1>
-            {postList.length === 0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick} />}
+            {postList.length === 0 && <WelcomeMessage />}
             <div className="flex flex-row flex-wrap-reverse">
                 {postList.map((post) => {
                     return <Post key={post.id} post={post} />;
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,9 +1,7 @@
-const WelcomeMessage = ({onGetPostsClick}) => {
+const WelcomeMessage = () => {
     return (
         <>
         <h1 className="text-white text-lg my-4">Currently their are No Posts!</h1>
-        <button onClick={onGetPostsClick}
-        className="bg-blue-700 text-white px-2 py-2 rounded-lg hover:bg-blue-600 transition-all duration-200">Get Post From Server</button>
         </>
     )
 };
